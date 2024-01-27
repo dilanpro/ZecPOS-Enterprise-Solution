622cff2efe6074175a0e3d71ae3220c183a3aa1e
dilan <dilan@prodoscore.com>
2024-01-27 14:10:11 +0530

    chore: flush

--- app/dashboard/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from .views import dashboard
-
-dash: Blueprint = Blueprint(name="dashboard", import_name=__name__, url_prefix="/")
-
-dash.add_url_rule(rule="/", view_func=dashboard.index, methods=["GET", "POST"])
